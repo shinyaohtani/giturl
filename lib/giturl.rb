55e92ff160b849c13abfe64d2ea3ae5a062a0b7d
Shinya Ohtani <shinya_ohtani@yahoo.co.jp>
2020-03-13 00:05:07 +0900

    Modularized main function

---
+++ lib/giturl.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+module Giturl
+  class Giturl
+    def self.git_managed?(path)
+      stderr_old = $stderr.dup
+      $stderr.reopen('/dev/null')
+      inside = `git -C #{path} rev-parse --is-inside-work-tree`.chomp
+      $stderr.flush
+      $stderr.reopen stderr_old
+      inside == 'true'
+    end
+
+    def self.url(path)
+      gitdir_prefix = `git -C #{path} rev-parse --show-prefix`.chomp
+      gitdir_branch = `git -C #{path} rev-parse --abbrev-ref HEAD`.chomp
+      remote_origin_url = `git -C #{path} config --get remote.origin.url`.chomp
+
+      # remote_origin_url:  git@github.com:shinyaohtani/giturl.git
+      baseurl = remote_origin_url.gsub(/:/, '/').gsub(/^.*@/, 'https://').gsub(/\.git$/, '')
+
+      "#{baseurl}/tree/#{gitdir_branch}/#{gitdir_prefix}"
+    end
+
+    def self.safe_url(path)
+      url(path) if git_managed?(path)
+    end
+  end
+end
