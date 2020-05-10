8422a93a2d024df4fad4b02e869cb4ab5a0c4c13
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2020-05-10 21:58:59 +0900

    refactored

--- lib/giturl/version.rb
+++ lib/giturl/version.rb
@@ -2,4 +2,16 @@
 
 module Giturl
   VERSION = '1.3.1'
+  DESCRIPTION = <<~DESC
+    A tiny helper for accessing GitHub web pages corresponding to local
+    working directories.
+
+    Have you ever wanted to access a GitHub web page while working on
+    a git-cloned local directory?
+    You can use 'giturl' to display the URL corresponding to the git-managed
+    directory given as an argument. And furthermore, if you want to access
+    the URL immediately with your browser, 'giturl' opens your browser and
+    automatically accesses the URL without your any operation on the browser.
+  DESC
+  REPOSITORY_URL = 'https://github.com/shinyaohtani/giturl'
 end
