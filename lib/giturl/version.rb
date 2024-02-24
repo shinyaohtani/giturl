a363b684257fb96ef3de5afec9aa34513f466853
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2024-02-24 22:48:49 +0900

    version

--- lib/giturl/version.rb
+++ lib/giturl/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Giturl
-  VERSION = '1.3.2'
+  VERSION = '1.3.3'
   DESCRIPTION = <<~DESC
     giturl: A simple navigation tool for GitHub pages from local git-cloned directories
 
