f2d3bac346c9f9754bbc14086ea2d54a7c8efb22
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2024-02-24 23:39:30 +0900

    typo

--- lib/giturl/version.rb
+++ lib/giturl/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Giturl
-  VERSION = '1.3.3'
+  VERSION = '1.3.4'
   DESCRIPTION = <<~DESC
-    giturl: A simple navigation tool for GitHub pages from local git-cloned directories
+    giturl: A simple navigation tool for GitHub pages from local git-cloned directories.
 
     Working deep in a git-cloned project and need to view its GitHub page?
     'giturl' simplifies this for you. A single command unveils your repository's URL.
